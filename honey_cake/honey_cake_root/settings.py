54a73bf464ca8f5e8184dec28822d23ae11fd170
GrzegorzGie21 <galazka.grzesiek@gmail.com>
2020-03-07 12:33:29 +0100

    Updated settings.py

--- honey_cake/honey_cake_root/settings.py
+++ honey_cake/honey_cake_root/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'honey_cake.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'honey_cake',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
     }
 }
 
